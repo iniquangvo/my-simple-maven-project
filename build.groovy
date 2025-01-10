8a670d7925136999b8b368314601d9ca9babdc71
quang.vo <quang.vo@spirent.com>
2025-01-10 13:10:35 +0700

    checkout from git, use withMaven (using plugin Pipeline Maven Integration)

--- build.groovy
+++ build.groovy
@@ -2,18 +2,29 @@ def build() {
     wsdir = pwd()
     stage('Preparation') {
         sh "echo ${wsdir}"
+        checkout([$class: 'GitSCM',
+            branches: [[name: '*/main']],
+            doGenerateSubmoduleConfigurations: false,
+            extensions: [],
+            submoduleCfg: [],
+            userRemoteConfigs: [[url: 'https://github.com/iniquangvo/my-simple-maven-project.git']]
+        ])
     }
     stage('Build') {
-        sh "mvn clean verify"
+        withMaven(maven: 'Maven 3.9.0') {
+            sh 'mvn clean verify'
+        }
     }
     stage('Test') {
-        sh "mvn clean package"
+        withMaven(maven: 'Maven 3.9.0') {
+            sh "mvn clean package"
+        }
     }
 }
 
 timeout(time: 12, unit: 'HOURS') {
     timestamps() {
-        node('linux') { //'ub-build-itest') {
+        node('linux') {
             build()
         }
     }
